09ea9bd071d826ca30bbc91c48696b1cb77933cb
Deividas <kliucevicd@gmail.com>
2024-07-21 12:10:25 +0300

    Mano automobilis

--- index.js
+++ index.js
@@ -23,7 +23,67 @@ console.log(brisius.birthday());
 console.log(brisius.birthday());
 console.log(brisius.birthday());
 
+console.clear();
 
+
+import { Car } from './js/Dog.js';
+const Bmw = new Car ('Bmw' , 'X5' ,'juodas' );
+console.log(Bmw);
+console.log(Bmw.engineStart());
+console.log(Bmw.engineStart());
+console.log(Bmw.startDriving(130));
+console.log(Bmw.engineRunning);
+console.log(Bmw.engineStop());
+console.log(Bmw.engineStop());
+console.log(Bmw.engineRunning);
+console.log(Bmw.engineStart());
+console.log(Bmw.startDriving(60));
+console.log(Bmw.speed);
+
+console.log(Bmw.engineStop());
+console.log(Bmw.engineStart());
+console.log(Bmw.startDriving(130));
+console.log(Bmw.drive(745));
+console.log(Bmw.refuelNeeded());
+console.log(Bmw.refuel(10.40));
+console.log(Bmw.canDriveDistance(100));
+console.log(Bmw.calculateFuelNeeded(1000));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class -> Car (Automobilis)
+ 
+// - pavadinimas (Audi)
+// - modelis (80)
+// - color
+// - kuro bako talpa (litrais)
+// - vidutines kuro sanaudos 100km
+// - ar ijungtas varyklis (default: false)
+// - greitis (default: 0)
+ 
+// metodai:
+// - ijungti varykli
+//     - ijungto varykli dar karto ijungti negalima, sugadinti starteri
+// - isjungti varykli
+//     - isjungto varyklio isjungti negalima...
+// - pradeti vaziuoti (greitis tiesiog tampa ne nulinis ir sunaudoja 2x litrus kuro sanaudu)
+// - vaziuoti (naudoja 1x litro kuro sanaudu)
+// - sustoti (greitis tiesiog tampa nulinis)
+// - kiek liko kuro?
+// - uzpilti kuro baka (kiek litrais)
+// has context menu
 
 
 
--- js/Dog.js
+++ js/Dog.js
@@ -32,3 +32,104 @@ manyBones(count){
     return `Many bones: ${`🦴`.repeat(count)}.`
 }
 }
+
+
+export class Car {
+    constructor ( marke , modelis , spalva){
+        this.make = marke 
+        this.model = modelis
+        this.color = spalva 
+        this.fuelTankCapacity = 90
+        this.averageFuelConsumption = 12; 
+        this.engineRunning = false
+        this.speed = 0
+        this.fuelLevel = this.fuelTankCapacity;
+        
+    }
+
+
+    engineStart() {
+        if (this.engineRunning) {
+            return `You already started the engine.`;
+        } else {
+            this.engineRunning = true;
+            
+            return `Brumm, engine started. Speed is now  0 km/h.`;
+        }
+    }
+
+    engineStop() {
+        if (!this.engineRunning) {
+            return `The engine is already stopped.`;
+        } else {
+            this.engineRunning = false;
+            this.speed = 0; // Reset speed to 0 km/h when the engine stops
+            return `Engine stopped. Speed is now ${this.speed} km/h.`;
+        }
+    }
+
+    isEngineRunning() {
+        return this.engineRunning;
+    }
+
+    getSpeed() {
+        return this.speed;
+    }
+    calculateFuelNeeded(distance) {
+        const fuelNeeded = (distance / 100) * this.averageFuelConsumption;
+        return fuelNeeded;
+    }
+
+    canDriveDistance(distance) {
+        const fuelNeeded = this.calculateFuelNeeded(distance);
+        if (fuelNeeded > this.fuelLevel) {
+            return `You need to refuel to drive ${distance} km. Fuel needed: ${fuelNeeded.toFixed(2)} liters.`;
+        } else {
+            return `You can drive ${distance} km. Fuel needed: ${fuelNeeded.toFixed(2)} liters.`;
+        }
+    }
+
+    drive(distance) {
+        const fuelNeeded = this.calculateFuelNeeded(distance);
+        if (fuelNeeded > this.fuelLevel) {
+            return `You don't have enough fuel to drive ${distance} km. Please refuel.`;
+        } else {
+            this.fuelLevel -= fuelNeeded;
+            return `You have driven ${distance} km. Remaining fuel: ${this.fuelLevel.toFixed(2)} liters.`;
+        }
+    }
+    refuelNeeded() {
+        const neededFuel = this.fuelTankCapacity - this.fuelLevel;
+        return `You need ${neededFuel.toFixed(2)} liters to refuel to a full tank.`;
+    }
+    refuel(amount) {
+        if (amount <= 0) {
+            return `Invalid refuel amount. Please provide a positive number.`;
+        }
+
+        const potentialFuelLevel = this.fuelLevel + amount;
+        if (potentialFuelLevel > this.fuelTankCapacity) {
+            const actualRefuelAmount = this.fuelTankCapacity - this.fuelLevel;
+            this.fuelLevel = this.fuelTankCapacity;
+            return `You have refueled ${actualRefuelAmount.toFixed(2)} liters. The tank is now full.`;
+        } else {
+            this.fuelLevel += amount;
+            return `You have refueled ${amount.toFixed(2)} liters. Current fuel level: ${this.fuelLevel.toFixed(2)} liters.`;
+        }
+    }
+    startDriving(speed) {
+        if (!this.engineRunning) {
+            return `Start the engine first.`;
+        }
+        if (speed <= 0) {
+            return `Invalid speed. Please provide a positive number.`;
+        }
+        this.speed = speed;
+        return `You are now driving at ${this.speed} km/h.`;
+    }
+}
+
+
+
+
+
